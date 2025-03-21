e5f16965d93e9dfbf85ee7a8a065f1a1db1bd47f
amanv-web <D22BCA110215@cuchd.in>
2025-03-21 21:29:18 +0530

    final

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -11,9 +11,9 @@ function HeroSection() {
           initial={{ y: "5vw", opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ type: "spring", duration: 2, delay: 0.5 }}
-          className="md:w-2/6"
+          className="md:w-2/6 w-11/12"
         >
-          <h1 className="md:text-3xl text-xl md:p-0 pt-20 pl-10 font-bold text-[#00FFFF]">
+          <h1 className="md:text-3xl   text-xl md:p-0 pt-20 pl-10 font-bold text-[#00FFFF]">
             Hey there!, I'm-
           </h1>
           <div>
